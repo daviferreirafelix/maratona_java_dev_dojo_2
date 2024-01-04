3f157916d3e714257ec54215e6c8de10b406ccb7
Davi <adavi9920@gmail.com>
2024-01-04 19:15:38 -0300

    Colecoes

--- dev_dojo/src/com/Ycolecao/dominio/Celular.java
+++ dev_dojo/src/com/Ycolecao/dominio/Celular.java
@@ -9,6 +9,30 @@ public class Celular {
         this.marca = marca;
     }
 
+    // Implementação do método equals deverá ser sempre:
+
+    // Reflexivo: x.equals(x) sempre deve retornar true
+    // Simetrico: se x.equals(y) for true, então y.equals(x) também é true
+    // Quando x for diferente de null, x.equals(null) retornara null
+    public boolean equals(Object obj){
+        if (obj == null) return false;
+        else if (this == obj) return true;
+        else if (this.getClass() != obj.getClass()) return false;
+
+        Celular cel = (Celular) obj;
+        return this.serialNumber != null && serialNumber.equals(cel.getSerialNumber());
+    }
+
+    // o atributo de comparação utilizado no hashCode deve ser o mesmo do equals
+    // se x.equals(y) == y.equals(x), então x.hashCode() == y.hashCode()
+    // se x.hashCode() == y.hashCode, não é necessario x.equals(y) == y.equals(x)
+    // se x.hashCode() != y.hashCode, logo x.equals(y) == false;
+    @Override
+    public int hashCode(){
+        return this.serialNumber == null ? 0 : this.serialNumber.hashCode();
+    }
+
+
     public String getSerialNumber() {
         return serialNumber;
     }
--- dev_dojo/src/com/Ycolecao/test/EqualsTest01.java
+++ dev_dojo/src/com/Ycolecao/test/EqualsTest01.java
@@ -1,7 +1,12 @@
 package com.Ycolecao.test;
 
+import com.Ycolecao.dominio.Celular;
+
 public class EqualsTest01 {
     public static void main(String[] args) {
+        Celular c1 = new Celular("123A", "iPhone");
+        Celular c2 = new Celular("123A", "iPhone");
 
+        System.out.println(c1.equals(c2));
     }
 }
