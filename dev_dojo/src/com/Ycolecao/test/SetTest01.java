fced0b075fd7d0b7421106246cbeb98754a69741
Davi <adavi9920@gmail.com>
2024-01-09 14:21:24 -0300

    Set and HashSet

--- dev_dojo/src/com/Ycolecao/test/SetTest01.java
+++ dev_dojo/src/com/Ycolecao/test/SetTest01.java
@@ -2,22 +2,23 @@ package com.Ycolecao.test;
 
 import com.Ycolecao.dominio.Manga;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-public class IteratorTest01 {
+public class SetTest01 {
     public static void main(String[] args) {
-        List<Manga> mangas = new LinkedList<>();
+        Set<Manga> mangas = new LinkedHashSet<>();
+
+        // Diferente do List, o Set não permite duplicação de dados
 
         mangas.add(new Manga(3L, "One Piece", 232L, 2));
         mangas.add(new Manga(0L, "Naruto", 210L, 4));
         mangas.add(new Manga(1L, "Dragon Ball", 234L, 1));
         mangas.add(new Manga(3L, "Beserk", 240L, 1));
+        mangas.add(new Manga(3L, "Beserk", 240L, 1));
 
-        System.out.println(mangas);
-
-        mangas.removeIf(manga -> manga.getQuantidade() == 1);
-
-        System.out.println(mangas);
+        for(Manga manga: mangas){
+            System.out.println(manga);
+        }
     }
 }
