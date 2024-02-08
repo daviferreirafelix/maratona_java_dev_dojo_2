9e5a60a9115f3b58a773ebd7112b3d2910f2e3e1
Davi <adavi9920@gmail.com>
2024-02-08 09:13:56 -0300

    Ganerics - Wildcard

--- dev_dojo/src/com/Zgenerics/test/WildcardTest01.java
+++ dev_dojo/src/com/Zgenerics/test/WildcardTest01.java
@@ -20,18 +20,36 @@ class Gato extends Animal{
 		System.out.println("Consultando gato");
 	}
 }
-
+	
 public class WildcardTest01 {
 	public static void main(String[] args) {
 		
-		List<Animal> animais = List.of(new Cachorro(), new Gato());
-		mostrarConsulta(animais);
+		List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
+		List<Gato> gatos = List.of(new Gato(), new Gato());
+		List<Animal> a = List.of(new Gato(), new Cachorro());
+		
+		
+		// exemplo do método com wildcard super
+		mostrarConsultaAnimal(a);
+		
+		// exemplos de métodos com wildcard extends
+		mostrarConsulta(gatos);
+		mostrarConsulta(cachorros);
 	}
-	
-	public static void mostrarConsulta(List<Animal> animals) {
+											// já o wildcard extends define que o tipo genérico só
+											// aceitará classes que estendem de Animals como parametro
+	public static void mostrarConsulta(List<? extends Animal> animals) {
 		for (Animal a: animals) {
 			a.consulta();
 		}
 	}
-
+												 // o wildcard super define que o tipo generico 
+												 // aceitará apenas classes superiores a Animal
+												 // como parametro
+	public static void mostrarConsultaAnimal(List<? super Animal> animals) {
+		for (Object o: animals) {
+			System.out.println(o);
+		}
+		
+	}
 }
